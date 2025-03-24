2d61e08a36cec5f706fb5a06a867d1a470da6470
João <jaolukas3101@gmail.com>
2025-03-24 17:47:30 -0300

    front links e rotas

---
+++ src/app/front-end/page.jsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import estilos from './frontend.module.css'
+
+export default function frontend() {
+    return(
+        <div>
+            <h2>React Js</h2>
+                <p>
+
+                </p>
+
+                <Link href='https://chatgpt.com' target="_blank">chatgpt</Link>
+        </div>
+    )
+}
